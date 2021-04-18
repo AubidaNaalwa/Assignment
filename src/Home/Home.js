342ba856569aec8a096706b7fe253f98c38c29d3
DrowRanger <54094000+AubidaNaalwa@users.noreply.github.com>
2021-04-19 02:05:24 +0300

    adding files

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,11 @@
 import { observer, inject } from 'mobx-react'
-
+import './Home.css'
 function Home(props) {
 
     return (
         <div className="Home">
             <h1>{props.userStore.username } You are Signed In</h1>
-            <button onClick={props.userStore.setLoggedOut}>LogOut</button>
+            <button className ="buttonLogOut" onClick={props.userStore.setLoggedOut}>LogOut</button>
         </div >
     );
 }
